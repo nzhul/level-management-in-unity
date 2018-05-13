a6080f5f30f512c839ea3b150b6d1d0b2de987e9
nzhul <dobromirivanov1@gmail.com>
2018-05-13 17:43:03 +0300

    starting work on save player progress functionality

--- SampleGame/Assets/LevelManagement/Scripts/Menus/SettingsMenu.cs
+++ SampleGame/Assets/LevelManagement/Scripts/Menus/SettingsMenu.cs
@@ -1,6 +1,82 @@
-﻿namespace LevelManagement
+﻿using LevelManagement.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LevelManagement
 {
     public class SettingsMenu : Menu<SettingsMenu>
     {
+        [SerializeField]
+        private Slider _masterVolumeSlider;
+
+        [SerializeField]
+        private Slider _sfxVolumeSlider;
+
+        [SerializeField]
+        private Slider _musicVolumeSlider;
+
+        private DataManager _dataManager;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _dataManager = FindObjectOfType<DataManager>();
+            LoadData();
+        }
+
+        public override void OnBackPressed()
+        {
+            base.OnBackPressed();
+
+            if (_dataManager != null)
+            {
+                _dataManager.Save();
+            }
+        }
+
+
+        public void OnMasterVolumeChanged(float volume)
+        {
+            if (_dataManager != null)
+            {
+                _dataManager.MasterVolume = volume;
+            }
+        }
+
+        public void OnSFXVolumeChanged(float volume)
+        {
+            if (_dataManager != null)
+            {
+                _dataManager.SfxVolume = volume;
+            }
+        }
+
+        public void OnMusicVolumeChanged(float volume)
+        {
+            if (_dataManager != null)
+            {
+                _dataManager.MusicVolume = volume;
+            }
+        }
+
+        public void LoadData()
+        {
+            if (_dataManager != null)
+            {
+                _dataManager.Load();
+            }
+
+            if (_dataManager == null ||
+                _masterVolumeSlider == null || 
+                _sfxVolumeSlider == null ||
+                _musicVolumeSlider == null)
+            {
+                return;
+            }
+
+            _masterVolumeSlider.value = _dataManager.MasterVolume;
+            _sfxVolumeSlider.value = _dataManager.SfxVolume;
+            _musicVolumeSlider.value = _dataManager.MusicVolume;
+        }
     }
 }
